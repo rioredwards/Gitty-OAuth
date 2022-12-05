4a49d623a44b2af1b19f220409de77636da5abb2
Rio Edwards <rioredwards@gmail.com>
2022-12-05 13:16:22 -0800

    Setup Repo

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,6 +1,8 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
-DROP TABLE IF EXISTS users;
+DROP TABLE IF EXISTS users CASCADE;
+
+DROP TABLE IF EXISTS github_users;
 
 CREATE TABLE users (
   id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -8,4 +10,11 @@ CREATE TABLE users (
   password_hash VARCHAR NOT NULL,
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL
+);
+
+CREATE TABLE github_users (
+  id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+  login TEXT NOT NULL,
+  email TEXT,
+  avatar TEXT
 );
